af499f13846addc18ff2917d3b9bafe4cd50484a
Wix Remote Machine <support@wix.com>
2026-04-04 05:51:48 +0000

    Atualizar GamePage.tsx

--- src/components/pages/GamePage.tsx
+++ src/components/pages/GamePage.tsx
@@ -1,11 +1,9 @@
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
+import Map3D from '@/components/Map3D';
 
 export default function GamePage() {
   return (
-    <div className="w-full h-screen relative overflow-hidden bg-black">
-      <Header />
-      <Footer />
+    <div className="w-full h-screen bg-black">
+      <Map3D />
     </div>
   );
 }
