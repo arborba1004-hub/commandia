2a36322a93cf01c74bb620225f140bba5770cb19
Wix Remote Machine <support@wix.com>
2026-04-04 17:51:03 +0000

    Ajustar Luz Ambiente

--- src/components/Map3D.tsx
+++ src/components/Map3D.tsx
@@ -149,7 +149,7 @@ export default function Map3D() {
     renderer.shadowMap.enabled = true;
     container.appendChild(renderer.domElement);
 
-    const ambientLight = new THREE.AmbientLight(0xffffff, 1.35);
+    const ambientLight = new THREE.AmbientLight(0xffffff, 2.5);
     scene.add(ambientLight);
 
     const dirLight = new THREE.DirectionalLight(0xffffff, 2.2);
