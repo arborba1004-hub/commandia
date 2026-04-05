eb5b6f8e59a84825e17301e6591a8b166e4ea68b
Wix Remote Machine <support@wix.com>
2026-04-05 00:49:00 +0000

    Inserir Header em Todas as Páginas

--- src/components/pages/GamePage.tsx
+++ src/components/pages/GamePage.tsx
@@ -1,9 +1,13 @@
+import Header from '@/components/Header';
 import Map3D from '@/components/Map3D';
 
 export default function GamePage() {
   return (
-    <div className="w-full h-screen bg-black">
-      <Map3D />
+    <div className="w-full h-screen bg-black flex flex-col">
+      <Header />
+      <div className="flex-1 overflow-hidden">
+        <Map3D />
+      </div>
     </div>
   );
 }
