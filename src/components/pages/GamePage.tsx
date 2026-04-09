0dd52b64d6e4d646e1aeed6ed79b7ac83f81d8f5
Wix Remote Machine <support@wix.com>
2026-04-09 02:37:54 +0000

    Use image for horizon

--- src/components/pages/GamePage.tsx
+++ src/components/pages/GamePage.tsx
@@ -36,7 +36,7 @@ const PLATFORM_HEIGHT = 1.2;
 const FLOOR_TEXTURE =
   'https://static.wixstatic.com/media/50f4bf_df004e568945465ba2231dc36addfe09~mv2.jpeg';
 const HORIZON_TEXTURE =
-  'https://static.wixstatic.com/media/50f4bf_ccd344777252495f9afc14f0bfa4548e~mv2.jpeg';
+  'https://static.wixstatic.com/media/50f4bf_20f0b19d58ab4edc9679d404e75059ae~mv2.jpeg';
 
 const BARRACO_MODELS = [
   { min: 1, max: 9, url: 'https://static.wixstatic.com/3d/50f4bf_78d8f707f621482698830308447c3ff2.glb' },
