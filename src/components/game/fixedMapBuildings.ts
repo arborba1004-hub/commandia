ef12096e772e53bf1d8bb63921ad10c11664c765
Wix Remote Machine <support@wix.com>
2026-04-23 01:54:17 +0000

    Atualizando rotas de edifícios

--- src/components/game/fixedMapBuildings.ts
+++ src/components/game/fixedMapBuildings.ts
@@ -23,7 +23,7 @@ export const FIXED_BUILDINGS: FixedBuildingConfig[] = [
     x: 0,
     z: 0,
     footprint: 10,
-    comingSoon: true,
+    route: '/treinamento-gang',
   },
 
   {
@@ -33,7 +33,7 @@ export const FIXED_BUILDINGS: FixedBuildingConfig[] = [
     x: -52,
     z: -52,
     footprint: 7,
-    route: '/gang',
+    route: '/treinamento-gang',
   },
   {
     key: 'ct_ne',
@@ -42,7 +42,7 @@ export const FIXED_BUILDINGS: FixedBuildingConfig[] = [
     x: 52,
     z: -52,
     footprint: 7,
-    route: '/gang',
+    route: '/treinamento-gang',
   },
   {
     key: 'ct_sw',
@@ -51,7 +51,7 @@ export const FIXED_BUILDINGS: FixedBuildingConfig[] = [
     x: -52,
     z: 52,
     footprint: 7,
-    route: '/gang',
+    route: '/treinamento-gang',
   },
   {
     key: 'ct_se',
@@ -60,7 +60,7 @@ export const FIXED_BUILDINGS: FixedBuildingConfig[] = [
     x: 52,
     z: 52,
     footprint: 7,
-    route: '/gang',
+    route: '/treinamento-gang',
   },
 
   {
