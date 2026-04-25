4bf1d40013bc9469a94ce7d11e1b9502c023c8a0
Wix Remote Machine <support@wix.com>
2026-04-25 03:20:53 +0000

    Resolvendo problemas do site

---
+++ src/integrations/index.ts
@@ -0,0 +1,5 @@
+// Re-export all integrations
+export { BaseCrudService } from './cms/service';
+export { useCart, useCartStore } from './cms/cms-ecom/cart';
+export { useCurrency, formatPrice, DEFAULT_CURRENCY } from './cms/cms-ecom/currency';
+export { buyNow } from './cms/cms-ecom/ecom-service';
