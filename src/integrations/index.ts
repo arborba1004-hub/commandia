58a8cec65a1e3194e22e5099f0842aa3eb342bef
Wix Remote Machine <support@wix.com>
2026-04-25 03:43:36 +0000

    Fix module export error

--- src/integrations/index.ts
+++ src/integrations/index.ts
@@ -3,14 +3,3 @@ export { BaseCrudService } from './cms/service';
 export { useCart, useCartStore } from './cms/cms-ecom/cart';
 export { useCurrency, formatPrice, DEFAULT_CURRENCY } from './cms/cms-ecom/currency';
 export { buyNow } from './cms/cms-ecom/ecom-service';
-
-// Ensure there's always a default export to prevent "no default export" errors
-export default {
-  BaseCrudService: require('./cms/service').BaseCrudService,
-  useCart: require('./cms/cms-ecom/cart').useCart,
-  useCartStore: require('./cms/cms-ecom/cart').useCartStore,
-  useCurrency: require('./cms/cms-ecom/currency').useCurrency,
-  formatPrice: require('./cms/cms-ecom/currency').formatPrice,
-  DEFAULT_CURRENCY: require('./cms/cms-ecom/currency').DEFAULT_CURRENCY,
-  buyNow: require('./cms/cms-ecom/ecom-service').buyNow,
-};
