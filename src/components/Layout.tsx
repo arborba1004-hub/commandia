69c8f427d431f0165502f97c2ab1b9e77d754c99
arborba1004-hub <arborba1004@gmail.com>
2026-04-26 06:22:03 -0300

    Atualizar o Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import AchievementNotification from '@/components/AchievementNotification';
 import { useGameSocket } from '@/hooks/useGameSocket';
-import { DebugLoader } from '@/components/DebugLoader';
+
 
 export default function Layout() {
   useGameSocket();
@@ -9,7 +9,7 @@ export default function Layout() {
   return (
     <div className="min-h-screen bg-background flex flex-col">
       <AchievementNotification />
-      <DebugLoader />
+      
       <main className="flex-1">
         <Outlet />
       </main>
