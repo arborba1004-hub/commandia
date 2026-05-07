5abfa4c7da1540ff8fd2c3b6d076daa83cfc4e79
Wix Remote Machine <support@wix.com>
2026-05-07 04:20:19 +0000

    Detectando GPU Fraca

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,17 @@
 import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import AchievementNotification from '@/components/AchievementNotification';
 import GameSocketBootstrap from '@/components/GameSocketBootstrap';
+import { applyGPUOptimizations } from '@/utils/gpuOptimization';
 
 export default function Layout() {
+  useEffect(() => {
+    // Apply GPU optimizations on app load
+    applyGPUOptimizations();
+  }, []);
+
   return (
     <div className="min-h-screen bg-background flex flex-col">
       <GameSocketBootstrap />
