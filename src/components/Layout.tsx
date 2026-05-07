54cf87a0ea419c2861302a1179a2d1beaa8da588
Wix Remote Machine <support@wix.com>
2026-05-07 03:22:47 +0000

    Atualizando Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,6 +2,7 @@ import { Outlet } from 'react-router-dom';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import AchievementNotification from '@/components/AchievementNotification';
+import GameSocketBootstrap from '@/components/GameSocketBootstrap';
 import { useGameSocket } from '@/hooks/useGameSocket';
 
 export default function Layout() {
@@ -10,6 +11,8 @@ export default function Layout() {
 
   return (
     <div className="min-h-screen bg-background flex flex-col">
+      <GameSocketBootstrap />
+      
       <Header />
       <AchievementNotification />
       
