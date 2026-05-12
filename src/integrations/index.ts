710e52110823461a4a45980f68bb1157bd286d0c
Wix Remote Machine <support@wix.com>
2026-05-12 02:26:28 +0000

    Corrigir problema de deploy no Wix

--- src/integrations/index.ts
+++ src/integrations/index.ts
@@ -2,7 +2,7 @@
 // NOTE: These are placeholder implementations for external backend integration
 // Replace with your own backend API calls
 
-export { BaseCrudService } from './cms/service';
-export { useCart, useCartStore } from './cms/cms-ecom/cart';
-export { useCurrency, formatPrice, DEFAULT_CURRENCY } from './cms/cms-ecom/currency';
-export { buyNow } from './cms/cms-ecom/ecom-service';
+export { BaseCrudService } from './cms/service.ts';
+export { useCart, useCartStore } from './cms/cms-ecom/cart.ts';
+export { useCurrency, formatPrice, DEFAULT_CURRENCY } from './cms/cms-ecom/currency.ts';
+export { buyNow } from './cms/cms-ecom/ecom-service.ts';
