41cc8010083675b36ca008741d8571046ee3eeaf
arborba1004-hub <arborba1004@gmail.com>
2026-05-21 02:16:13 -0300

    Add files via upload

---
+++ src/data/azideiaCatalog.ts
@@ -0,0 +1,6 @@
+export const AZIDEIA_X9_MODEL_URL = 'https://static.wixstatic.com/3d/50f4bf_d07bb1c9327e4f0aaec0681e47314a9e.glb';
+export const AZIDEIA_ICON_URL = 'https://static.wixstatic.com/media/50f4bf_ce2c97a1cf324091851178166ed02d29~mv2.png';
+
+export const AZIDEIA_X9_COST_DIRTY = 5000;
+export const AZIDEIA_X9_DAILY_LIMIT = 20;
+export const AZIDEIA_ACTIVE_X9_COUNT = 10;
