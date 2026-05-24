a3420015c4ce4540db488b39402d3561f8bc7fd9
arborba1004-hub <arborba1004@gmail.com>
2026-05-24 17:18:50 -0300

    Add files via upload

--- src/data/azideiaCatalog.ts
+++ src/data/azideiaCatalog.ts
@@ -1,6 +1,13 @@
 export const AZIDEIA_X9_MODEL_URL = 'https://static.wixstatic.com/3d/50f4bf_d07bb1c9327e4f0aaec0681e47314a9e.glb';
+export const AZIDEIA_CORRERIA_MODEL_URL = 'https://static.wixstatic.com/3d/50f4bf_20d89e99d4084eb4a12aea96fa04556d.glb';
 export const AZIDEIA_ICON_URL = 'https://static.wixstatic.com/media/50f4bf_ce2c97a1cf324091851178166ed02d29~mv2.png';
+export const AZIDEIA_CORRERIA_ICON_URL = 'https://static.wixstatic.com/media/50f4bf_9bda4af1a12b47679336479a80b16eb8~mv2.png';
 
 export const AZIDEIA_X9_COST_DIRTY = 5000;
 export const AZIDEIA_X9_DAILY_LIMIT = 20;
 export const AZIDEIA_ACTIVE_X9_COUNT = 20;
+
+export const AZIDEIA_CORRERIA_COST_DIRTY = 0;
+export const AZIDEIA_CORRERIA_DAILY_LIMIT = 10;
+export const AZIDEIA_ACTIVE_CORRERIA_COUNT = 10;
+export const AZIDEIA_CORRERIA_FACTION_DAILY_LIMIT = 100;
